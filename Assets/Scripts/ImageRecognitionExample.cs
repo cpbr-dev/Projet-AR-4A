ea53ff203ae4d23ad6e5fe39df8398881cb3ca96
Munster <nael.messaoud06@gmail.com>
2023-02-16 16:41:21 +0100

    First commit

---
+++ Assets/Scripts/ImageRecognitionExample.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageRecognitionExample : MonoBehaviour
+{
+    private ARTrackedImageManager _aRTrackedImageManager; //Reference au script de gestion
+    private void Awake()
+    {
+        _aRTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+    private void OnEnable()
+    {
+        _aRTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+    public void OnDisable()
+    {
+        _aRTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+    // Cette fonction permet de determiner ce qui se passe lorsqu'une image est detectee
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var trackedImage in args.added)
+        {
+            Debug.Log(trackedImage.name);
+        }
+    }
+}
